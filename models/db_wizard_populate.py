de4f2b1f0512d2803ce38c3b57ee4bd25246343b
Marko <regs@sofistes.net>
2016-01-16 21:50:51 +0200

    Update db_wizard_populate.py
    Removed automatic population of auth_user and t_kiinnostuneet.

--- models/db_wizard_populate.py
+++ models/db_wizard_populate.py
@@ -1,4 +1,4 @@
-from gluon.contrib.populate import populate
-if db(db.auth_user).isempty():
-     populate(db.auth_user,10)
-     populate(db.t_kiinnostuneet,10)
+#from gluon.contrib.populate import populate
+#if db(db.auth_user).isempty():
+#     populate(db.auth_user,10)
+#     populate(db.t_kiinnostuneet,10)
